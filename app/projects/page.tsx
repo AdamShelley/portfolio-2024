347351b35403a31e382a2d53c839a76ff7d4f359
Adam Shelley <adam.shelley18@gmail.com>
2024-07-03 06:55:51 +0100

    mdx layouts using custom next Image now.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Projects = (props: Props) => {
   return (
     <div className="w-[85vw] lg:w-[50vw] xl:w-[45vw] 2xl:w-[30vw]">
-      <h2 className="mb-10 text-primary-muted font-base text-primary">
+      <h2 className="mb-10 text-primary-muted font-base dark:text-gray-300">
         Projects
       </h2>
       <ProjectList />

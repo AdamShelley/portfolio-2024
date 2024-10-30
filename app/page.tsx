b343a275799161323d6eb508167abdade02262f2
Adam Shelley <adam.shelley18@gmail.com>
2024-10-30 09:29:08 +0800

    Updating the animation delay

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import BlogLinks from "./(bloglinks)/bloglinks";
 export default function Home() {
   return (
     <main className="w-[85vw] lg:w-[50vw] xl:w-[45vw] 2xl:w-[30vw] mt-10 text-primary translate-x-[-0.5rem] flex flex-col orchestration">
-      <div className="p-3 group flex flex-col w-full rounded-md  transition duration-200">
+      <div
+        className="p-3 group flex flex-col w-full rounded-md  transition duration-200"
+        style={{ "--stagger": 1 } as React.CSSProperties}
+      >
         <h1 className="text-lg dark:text-gray-400 mb-5">About</h1>
         <p className="text-lg text-left  text-gray-800 dark:text-gray-300 leading-7 ">
           My name is{" "}
@@ -25,7 +28,7 @@ export default function Home() {
 
       <Link
         href="/projects"
-        style={{ "--stagger": 1 } as React.CSSProperties}
+        style={{ "--stagger": 2 } as React.CSSProperties}
         className="mt-10"
       >
         <div className="p-3 group flex flex-col w-full hover-bg rounded-sm cursor-pointer transition duration-200">

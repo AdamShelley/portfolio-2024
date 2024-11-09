e67a83ea2ec7b6bc9ba4ac43623485baf4faa38f
Adam Shelley <adam.shelley18@gmail.com>
2024-11-09 16:24:37 +0800

    Updating for mobile styles on blurred top bar.

--- components/blurred-top.tsx
+++ components/blurred-top.tsx
@@ -1,7 +1,7 @@
 const BlurredTopBar = () => {
   return (
     <div
-      className="fixed top-0 left-0 w-full h-[5rem] z-50 pointer-events-none blur-top"
+      className="fixed top-0 left-0 w-full h-[3rem] md:h-[5rem] z-50 pointer-events-none blur-top"
       style={{}}
     />
   );

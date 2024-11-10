facab413238441c30f28a02e40526749f1bec0f4
Adam Shelley <adam.shelley18@gmail.com>
2024-11-10 16:39:26 +0800

    Mode switcher styling

--- components/blurred-top.tsx
+++ components/blurred-top.tsx
@@ -1,9 +1,6 @@
 const BlurredTopBar = () => {
   return (
-    <div
-      className="fixed top-0 left-0 w-full h-[3rem] md:h-[5rem] z-50 pointer-events-none blur-top"
-      style={{}}
-    />
+    <div className="fixed top-0 left-0 w-full h-[3rem] md:h-[5rem] z-50 pointer-events-none blur-top" />
   );
 };
 

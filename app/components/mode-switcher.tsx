00192a0c460e7c3d0ed468e9b3fed820f46de61d
Adam Shelley <adam.shelley18@gmail.com>
2025-04-03 11:06:21 +0800

    Fixed routing issue

--- app/components/mode-switcher.tsx
+++ app/components/mode-switcher.tsx
@@ -36,7 +36,7 @@ export function ModeToggle() {
   return (
     <button
       onClick={handleThemeToggle}
-      className="relative z-10 text-slate-500 bg-slate-300 dark:bg-transparent  dark:text-slate-100 flex items-center justify-center self-center w-10 h-10 ml-8 transition-colors duration-300 ease-in-out rounded-full  bg-text hover:opacity-90 active:scale-95"
+      className="relative z-10 text-slate-900 bg-transparent dark:text-slate-100 flex items-center justify-center self-center w-10 h-10 ml-8 transition-colors duration-300 ease-in-out rounded-full  bg-text hover:opacity-90 active:scale-95"
       aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
     >
       {theme === "dark" ? (

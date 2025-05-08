8e4af9d1506d8cec322d7baec2675a7982d8ae08
Adam Shelley <adam.shelley18@gmail.com>
2025-05-08 09:36:00 +0800

    Updating version of framer to work with vercel.

--- app/components/mdx-remote.tsx
+++ app/components/mdx-remote.tsx
@@ -16,7 +16,7 @@ import { SearchBar } from "./SearchBar";
 const ProseExemptSearchBar = (props: any) => (
   <div className="not-prose search-bar !p-0 !m-0 !my-8 ">
     <div className="[&_*]:!leading-normal [&_a]:!no-underline [&_p]:!m-0 [&_span]:!m-0">
-      <SearchBar {...props} />
+      <SearchBar {...props} darkMode={true} />
     </div>
   </div>
 );

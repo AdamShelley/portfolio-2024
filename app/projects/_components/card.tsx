7cd19eabfa3a590e1ae675b72212331c5757d87a
Adam Shelley <adam.shelley18@gmail.com>
2025-05-19 09:04:47 +0800

    Separating the link and page name for more flexibility.

--- app/projects/_components/card.tsx
+++ app/projects/_components/card.tsx
@@ -6,16 +6,16 @@ import { Badge } from "@/components/ui/badge";
 interface Project {
   project: {
     name: string;
+    filename: string;
     skills: string[];
     description: string;
-    imageName?: string;
   };
   customPage?: string;
   stagger: number;
 }
 
 const Card = ({ project, customPage, stagger }: Project) => {
-  const { name, skills, description, imageName } = project;
+  const { name, skills, description, filename } = project;
 
   const capitalizeFirstLetter = (name: string) => {
     return name.charAt(0).toUpperCase() + name.slice(1);
@@ -23,52 +23,8 @@ const Card = ({ project, customPage, stagger }: Project) => {
 
   return (
     <>
-      {/* <article className="hidden dark:flex flex-col px-0 py-3 lg:px-0 lg:py-0 items-center justify-start w-full transition-all ease-in-out duration-300 rounded-sm relative overflow-hidden  m-0.5 lg:w-full  dark:shadow-none">
-        <div className="relative w-full h-[250px] rounded-md overflow-hidden bg-transparent border border-slate-200 shadow-md dark:border-none">
-          <Link href={customPage || `/projects/${name}`} passHref>
-            <div className="w-full h-full relative block cursor-pointer group ">
-              <Image
-                src={`/assets/${imageName}`}
-                alt={`${name} picture`}
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-                className="transition-opacity duration-300 ease-in-out dark:opacity-50 bg-transparent group-hover:opacity-90 dark:group-hover:opacity-30"
-              />
-              <div
-                className="absolute top-0 left-0 w-full h-full pointer-events-none 
-            bg-gradient-to-t from-black/90 via-black/70 to-transparent dark:bg-custom-gradient group-hover:from-black/70 group-hover:via-black/50 
-            dark:group-hover:bg-custom-gradient-hover 
-            transition-all duration-300 ease-in-out z-10 opacity-85 group-hover:opacity-80"
-              ></div>
-
-              <div className="absolute inset-x-0 bottom-0 flex flex-col justify-start flex-1 w-full p-3 ">
-                <div className="p-3 text-white z-20 absolute bottom-0 mb-2 ">
-                  <h3 className="text-xl mt-1 font-semibold text-white">
-                    {capitalizeFirstLetter(name)}
-                  </h3>
-                  <p className="mt-3 flex-1 text-sm tracking-normal text-gray-100 leading-5">
-                    {description}
-                  </p>
-                  <div className="flex flex-wrap items-center justify-start w-full pt-1 z-10 -ml-1">
-                    {skills &&
-                      skills.map((skill, index) => (
-                        <Badge
-                          key={index}
-                          className="mr-2 mt-2 min-w-[50px] text-[10px] font-normal bg-white/20 text-white dark:bg-teal-600/10 dark:text-teal-300/90 dark:border-teal-700 rounded-xs"
-                        >
-                          {skill}
-                        </Badge>
-                      ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </div>
-      </article> */}
       <article className="flex flex-col ">
-        <Link href={customPage || `/projects/${name}`} passHref>
+        <Link href={customPage || `/projects/${filename}`} passHref>
           <h3 className="hover:text-zinc-400 transition-all">
             {capitalizeFirstLetter(name)}
           </h3>
--- app/projects/_components/index.tsx
+++ app/projects/_components/index.tsx
@@ -6,59 +6,59 @@ const IS_DEV = process.env.NODE_ENV === "development";
 const projects = [
   {
     project: {
-      name: "Search-bar",
+      name: "Search Bar",
+      filename: "search-bar",
       skills: ["ReactJS", "Motion", "Tailwind"],
       description: "A search bar component for React/Next.",
-      imageNamE: "search-bar-screenshot.png",
       published: false,
     },
   },
   {
     project: {
       name: "Git-Pulse",
+      filename: "git-pulse",
       skills: ["Tauri", "Typescript", "React", "Rust"],
       description: "A minimal GitHub desktop client",
-      imageName: "gitpulse-screenshot.png",
       published: true,
     },
   },
   {
     project: {
       name: "electron-tailwind-template",
+      filename: "electron-tailwind-template",
       skills: ["Electron", "Tailwind", "Shadcn", "Template"],
       description:
         "A template for creating electron apps with tailwind and shadcn.",
-      imageName: "template-screenshot.png",
       published: true,
     },
   },
   {
     project: {
       name: "D-Struct",
+      filename: "d-struct",
       skills: ["NextJS", "Typescript", "Cloudflare", "Drizzle"],
       description:
         "A single use self-destructing file sharing platform built with NextJS.",
-      imageName: "dstruct-screenshot.png",
       published: true,
     },
   },
   {
     project: {
       name: "breathe",
+      filename: "breathe",
       skills: ["ReactJS", "Typescript", "Raycast"],
       description:
         "A small extension for Raycast, to give you quick, and simple mindfulness exercises.",
-      imageName: "breathe-screenshot.png",
       published: true,
     },
   },
   {
     project: {
       name: "langAI",
+      filename: "lang-ai",
       skills: ["React Native", "NodeJS", "Supabase", "ChatGPT"],
       description:
         "A language learning app that uses AI to generate stories for your level.",
-      imageName: "LangAI-screenshot.png",
       published: true,
     },
   },

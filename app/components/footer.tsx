d7dfb92abab8e877f47883356a194c0fbb47f08a
Adam Shelley <adam.shelley18@gmail.com>
2025-11-18 10:18:26 +0800

    Refactor footer component and update blog post bittorent part 2.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,18 +4,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full text-sm flex items-center justify-evenly border-t border-gray-300 dark:border-gray-600 ">
-      <div className="w-[85vw] lg:w-[50vw] xl:w-[45vw] 2xl:w-[30vw] flex items-center justify-between my-1">
-        <span className="dark:text-gray-400">Adam Shelley</span>
+    <footer className="w-full text-sm flex items-center justify-evenly border-t border-gray-300 dark:border-transparent ">
+      <div className="w-[85vw] lg:w-[50vw] xl:w-[45vw] 2xl:w-[30vw] my-1">
+        {/* <span className="dark:text-gray-400">Adam Shelley</span> */}
         <Link
           href="https://github.com/AdamShelley"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="github link"
+          className="flex items-center justify-end"
         >
+          <span className="dark:text-gray-400">GitHub</span>
           <Github
             size={35}
-            className="text-gray-500 dark:text-gray-400 hover:dark:text-gray-300 transition-all ease-in rounded-full p-2"
+            className="text-gray-500 dark:text-gray-400 hover:dark:text-gray-300 transition-all ease-in rounded-full p-2 pr-0"
           />
         </Link>
       </div>
